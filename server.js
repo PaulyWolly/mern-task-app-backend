11c3a77a00b969e6b7a455191f2ac4ec82854fbc
Paul Welby <pwelby@gmail.com>
2023-07-04 13:52:14 -0700

    updated CORS in server.js

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors(
- {
-  origin: [
-    "http://localhost:3000",
-    "https://mern-task-app-backend-jqo4.onrender.com"
-  ]
- }
+//  {
+//   origin: [
+//     "http://localhost:3000",
+//     "https://mern-task-app-api-92jj.onrender.com"
+//   ]
+//  }
 ));
 
 
